7681fdc2c0b964c185103a5fc7f4ee850abb6ad1
unknown <prashantborade321@gmail.com>
2022-11-26 15:37:16 +0530

    Action method added in util class

--- src/main/java/utilClasses/UtilClass.java
+++ src/main/java/utilClasses/UtilClass.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -44,6 +45,11 @@ public class UtilClass {
 		Properties prop = new Properties();
 		prop.load(file);
 		return prop.getProperty(key);
+	}
+	
+	public void hoverMethod(WebDriver driver,WebElement element) {
+		Actions act = new Actions(driver);
+		act.moveToElement(element);
 	}
 
 }
